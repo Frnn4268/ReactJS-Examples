76afd53c7daff245c2c868aa144848f18f8faa43
Frnn4268 <appstutorialesy@gmail.com>
2023-12-16 18:45:24 -0600

    Updating more old content of this repository

--- frontend/src/Pages/Form.jsx
+++ frontend/src/Pages/Form.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DynamicForm from './DynamicForm';
+import '../css/Form.css'; // Import the CSS file for styling
 
 const campos = [
-    { label: 'Nombre', name: 'nombre', type: 'text' },
-    { label: 'Correo Electrónico', name: 'email', type: 'email' },
-    { label: 'Edad', name: 'edad', type: 'number' },
-    { label: 'Comentario', name: 'comentario', type: 'textarea' } , ]
+  { label: 'Name: ', name: 'name', type: 'text' },
+  { label: 'Email: ', name: 'email', type: 'email' },
+  { label: 'Age: ', name: 'age', type: 'number' },
+  { label: 'Comment: ', name: 'comment', type: 'textarea' },
+];
 
 export const Form = () => {
-    const handleSubmit = (formData) => {
-        console.log('Form Data:', formData);
-    }
+  const [submittedData, setSubmittedData] = useState(null);
 
-   return (
+  const handleSubmit = (formData) => {
+    setSubmittedData(formData);
+
+    // Hide the data after 5 seconds
+    setTimeout(() => {
+      setSubmittedData(null);
+    }, 5000);
+  };
+
+  return (
     <div>
-      <h1>Formulario Dinámico</h1>
+      <h1>Dynamic Form</h1>
       <DynamicForm fields={campos} onSubmit={handleSubmit} />
+
+      {/* Display submitted data */}
+      {submittedData && (
+        <div className="submitted-data-container">
+          <h2>Submitted Data:</h2>
+          <pre>{JSON.stringify(submittedData, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
-}
+};
